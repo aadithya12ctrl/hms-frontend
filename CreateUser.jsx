0035a3c0f0ddd17783fe9473d55614a320a82f6f
aadithya12ctrl <sheshadrimv999@gmail.com>
2025-11-30 12:20:27 +0530

    src/pages/users

---
+++ CreateUser.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const CreateUser = () => {
+  return <Typography>Create User - Coming Soon</Typography>;
+};
+
+export default CreateUser;
---
+++ UserList.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const UserList = () => {
+  return <Typography>User Management - Coming Soon</Typography>;
+};
+
+export default UserList;
