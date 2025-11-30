de9097549c14cc7b048cf205201f2382eff166c0
aadithya12ctrl <sheshadrimv999@gmail.com>
2025-11-30 12:21:04 +0530

    src/redux

---
+++ store.js
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice';
+import patientReducer from './slices/patientSlice';
+import uiReducer from './slices/uiSlice';
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    patient: patientReducer,
+    ui: uiReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export default store;
