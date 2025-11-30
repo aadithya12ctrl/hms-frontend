dccbd36cb2ab512979187d651cba39e3a11c4499
aadithya12ctrl <sheshadrimv999@gmail.com>
2025-11-30 12:12:02 +0530

    src

---
+++ main.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import App from './App';
+import store from './redux/store';
+import theme from './theme/theme';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+          toastStyle={{
+            backgroundColor: '#1A1A1A',
+            border: '1px solid rgba(212, 175, 55, 0.3)',
+            color: '#FFFFFF',
+          }}
+        />
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>
+);
